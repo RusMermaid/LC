61dbc2c80e909da87354a7009d4fb758156bb5a3
RusMermaid <ruslanf743@gmail.com>
2024-11-08 18:38:55 +0300

    Lambda Calculus Console App and Lib
    FINAL

--- Lambda_Calculus_App/Lambda_Calculus_App/Main.cs
+++ Lambda_Calculus_App/Lambda_Calculus_App/Main.cs
@@ -6,7 +6,7 @@ internal class MainClass
 {
     public static void Main(string[] args)
     {
-        string lambda_string = @"λx.λy.λz.x z (y z)"; //S combinator
+        const string lambda_string = @"λx.λy.λz.x z (y z)"; //S combinator
         LambdaExpression lambda_exp = LambdaAnaliz.LambdaParser.ParseTerm(lambda_string);
         Console.WriteLine(lambda_exp.PrintBinary());
     }
